ecfab6fc5a936690c0cef8bbedd3833730179a3a
Tron Legacy <tronlegacy@Trons-iMac.local>
2021-02-08 10:32:43 +0000

    added command Payment Command Services

--- src/PaymentSystems.WebAPI/Application/PaymentCommandServices.cs
+++ src/PaymentSystems.WebAPI/Application/PaymentCommandServices.cs
@@ -2,23 +2,25 @@ using PaymentSystems.FrameWork;
 using PaymentSystems.Domain;
 using PaymentSystems.Contract;
 using PaymentSystems.Domain.Payments;
+using PaymentSystems.Domain.Accounts;
 
 
 namespace PaymentSystems.WebAPI.Application
 {
     public class PaymentCommandService :  CommandService<Payment, PaymentId, PaymentState> {
-        public PaymentCommandService(IAggregateStore store, IsRoomAvailable isRoomAvailable, ConvertCurrency convertCurrency) : base(store) {
+        public PaymentCommandService(IAggregateStore store) : base(store) {
             OnNew<PaymentCommands.Submit>(
                  (payment, cmd) =>
-                    payment.SubmitPayment(new AccountId(), new PaymentId(), cmd.Amount));
+                    payment.SubmitPayment(new AccountId(cmd.AccountId), new PaymentId(cmd.PaymentId), cmd.Amount));
 
             OnExisting<PaymentCommands.Approve>(
+                 cmd => new PaymentId(cmd.PaymentId),
               (payment, cmd) =>
-                    payment.ApprovePayment(cmd.Amount, new AccountId()));
-            
-                (payment, cmd) =>
+                    payment.ApprovePayment(cmd.Amount, new AccountId(cmd.AccountId)));
             OnExisting<PaymentCommands.Execute>(
-                    payment.ExecutePayment(cmd.Amount, new AccountId()));
+                 cmd => new PaymentId(cmd.PaymentId),
+                (payment, cmd) =>
+                    payment.ExecutePayment(cmd.Amount, new AccountId(cmd.AccountId)));
         }
     }
 }
